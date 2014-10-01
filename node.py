fdbf5580242c7199a89a5f91651a44c9fcc583ef
rye_yawn <rye_yawn@rye.yawn-Inspiron-5423>
2014-10-01 10:50:05 -0500

    Added node class

---
+++ node.py
@@ -0,0 +1,10 @@
+class node: 
+
+	#The ever so sexy container for each vertex on graph
+
+	vertex = (0,0) #Location of node
+	neighbors = [] #Contains index for neighboring nodes in graph
+	distance = "inf" #Distance value from beginning.  Default is infinity...Babay...
+	previous = "undef" #Saves index of previous node to help keep track of path.
+	visit_status = 0 #0 means unvisited. 1 mean visited.  
+
