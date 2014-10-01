f1ed1e15d8a033955ae2223e6a1d6ef23ad71c59
rye_yawn <rye_yawn@rye.yawn-Inspiron-5423>
2014-10-01 11:23:33 -0500

    Less initial parameters.

--- node.py
+++ node.py
@@ -2,11 +2,11 @@ class Node:
 
 	#The ever so sexy container for each vertex on graph
 
-	def __init__(self, vertex=(0,0), neighbors=[], distance=float("inf"), previous="undef", visit_status=0):
+	def __init__(self, vertex, neighbors=[]):
 
 		self.vertex = vertex #Location of node
 		self.neighbors = neighbors #Contains index for neighboring nodes in graph
-		self.distance = distance #Distance value from beginning.  Default is infinity...Babay...
-		self.previous = previous #Saves index of previous node to help keep track of path.
-		self.visit_status = visit_status #0 means unvisited. 1 mean visited.  
+		self.distance = float("inf") #Distance value from beginning.  Default is infinity...Babay...
+		self.previous = "undef" #Saves index of previous node to help keep track of path.
+		self.visit_status = 0 #0 means unvisited. 1 mean visited.  
 
