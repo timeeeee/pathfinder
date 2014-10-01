a67c5c06a1568f527607b4ea6ff7b354156f094b
rye_yawn <rye_yawn@rye.yawn-Inspiron-5423>
2014-10-01 11:14:24 -0500

    Fixed node class (forgot __init__ and self)

--- node.py
+++ node.py
@@ -2,9 +2,11 @@ class Node:
 
 	#The ever so sexy container for each vertex on graph
 
-	vertex = (0,0) #Location of node
-	neighbors = [] #Contains index for neighboring nodes in graph
-	distance = float("inf") #Distance value from beginning.  Default is infinity...Babay...
-	previous = "undef" #Saves index of previous node to help keep track of path.
-	visit_status = 0 #0 means unvisited. 1 mean visited.  
+	def __init__(self, vertex=(0,0), neighbors=[], distance=float("inf"), previous="undef", visit_status=0):
+
+		self.vertex = vertex #Location of node
+		self.neighbors = neighbors #Contains index for neighboring nodes in graph
+		self.distance = distance #Distance value from beginning.  Default is infinity...Babay...
+		self.previous = previous #Saves index of previous node to help keep track of path.
+		self.visit_status = visit_status #0 means unvisited. 1 mean visited.  
 
