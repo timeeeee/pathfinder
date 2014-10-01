0d15b6653691486b3aad76e65327ae1ec05beeec
rye_yawn <rye_yawn@rye.yawn-Inspiron-5423>
2014-10-01 11:00:54 -0500

    Used float(inf) rather than inf

--- node.py
+++ node.py
@@ -4,7 +4,7 @@ class Node:
 
 	vertex = (0,0) #Location of node
 	neighbors = [] #Contains index for neighboring nodes in graph
-	distance = "inf" #Distance value from beginning.  Default is infinity...Babay...
+	distance = float("inf") #Distance value from beginning.  Default is infinity...Babay...
 	previous = "undef" #Saves index of previous node to help keep track of path.
 	visit_status = 0 #0 means unvisited. 1 mean visited.  
 
